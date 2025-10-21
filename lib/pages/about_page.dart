2f644da04a5ac4b0630838cd69130e1987fbaf3c
rochelleerfe <erferochelle0104@gmail.com>
2025-10-21 21:59:10 +0800

    Update about_page.dart

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("About Me po"),
+        title: Text('Welcome Aboutpage'),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://wallpaperaccess.com/full/5653640.jpg',
+                  width: 1000,
+                  height: 500,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Annyeonghaseyo!! Im Rochelle and i am currently third year student',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ]
+          )
+      ),
     );
+
+
   }
 }
