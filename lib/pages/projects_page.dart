c9e59a13c3701cbcc6fa114205826f54bc649a21
rochelleerfe <erferochelle0104@gmail.com>
2025-10-21 22:02:03 +0800

    Update projects_page.dart

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ProjectsPage extends StatelessWidget {
-    @override
+class ProjectPage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Projects po"),
+        title: Text('Welcome Projectpage'),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png',
+                  width: 1000,
+                  height: 500,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'My project for Sysarc is web-based',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ]
+          )
+      ),
     );
+
+
   }
 }
